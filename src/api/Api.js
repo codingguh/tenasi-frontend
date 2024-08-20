d85924278bb8f4942e22105ef2a9a537bae79041
codingguh <teguhharits91@gmail.com>
2024-08-21 01:02:57 +0700

    again

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://tenasi.teguhharits.my.id'
 })
 
 export default Api
